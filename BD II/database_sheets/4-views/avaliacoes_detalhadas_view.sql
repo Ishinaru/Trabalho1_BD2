e89dd5c51d8145be1fea7b24e59d8e4cb1a72e8f
Ishinaru <dvplima.cic@uesc.br>
2024-05-02 03:16:11 -0300

    fixing view

--- BD II/database_sheets/4-views/avaliacoes_detalhadas_view.sql
+++ BD II/database_sheets/4-views/avaliacoes_detalhadas_view.sql
@@ -1,5 +1,5 @@
 CREATE VIEW AvaliacoesDetalhadas AS
-SELECT A.LivroID, L.Titulo, U.Nome, A.Nota, A.Comentario, A.DataAvaliacao
+SELECT U.UsuarioID, A.LivroID, L.Titulo, U.Nome, A.Nota, A.Comentario, A.DataAvaliacao
 FROM Avaliacoes A
 JOIN Livros L ON A.LivroID = L.LivroID
 JOIN Usuarios U ON A.UsuarioID = U.UsuarioID;
